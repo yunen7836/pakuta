1e661d1fd1d4512ad6f76aed64c3a0282483f761
yunen7836 <yunen7836@yahoo.co.jp>
2020-01-03 15:48:59 +0900

    fonts_and_errorr_result

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,6 +2,9 @@ class RankingController < ApplicationController
 
   def likesorder
     @questions = Question.create_likesorder
+    if @question.blank?
+      @questions = Question.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    end
   end
 
 end
--- app/controllers/showlikesorder_controller.rb
+++ app/controllers/showlikesorder_controller.rb
@@ -4,8 +4,14 @@ class ShowlikesorderController < ApplicationController
     @question = Question.find(params[:question_id])
     @likes = Like.where(question_id: params[:question_id])
     @likes_top = Boke.find(@likes.group(:boke_id).order('count(boke_id) desc').limit(1).pluck(:boke_id))
-    @boke = Boke.new
-    @bokes = Boke.find(Like.where(question_id: params[:question_id]).group(:boke_id).order('count(boke_id) desc').limit(10).pluck(:boke_id))
+    if @likes_top.blank?
+      @likes_top = Boke.where(question_id: params[:question_id]).order("created_at asc").limit(1)
+      @boke = Boke.new
+      @bokes = @question.bokes.includes(:user).order("created_at asc").page(params[:page]).per(5)
+    else
+      @boke = Boke.new
+      @bokes = Boke.find(Like.where(question_id: params[:question_id]).group(:boke_id).order('count(boke_id) desc').limit(10).pluck(:boke_id))
+    end
   end
 
 end
