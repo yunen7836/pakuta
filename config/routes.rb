7fcf4f7a8b74a44abde2b548df1e5cc4c2b4b16b
yunen7836 <yunen7836@yahoo.co.jp>
2020-01-03 12:20:39 +0900

    show_likes_order_and_preview

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
   get 'ranking/likesorder'
+
   resources :questions do
+    get 'showlikesorder/showlikesorder'
     resources :bokes, only: :create do
       resources :likes, only: [:create, :destroy]
     end
